ce27df815becee16f03e77c3ed3bae910757b6ec
zoel-a <zulmyazhary32@gmail.com>
2023-03-19 14:05:11 +0800

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import type { Component } from "solid-js";
+
+const App: Component = () => {
+  return (
+    <>
+      <header>
+        Header
+      </header>
+      <main>
+        <h1 class="text-softBlue text-xl font-medium">Hello World</h1>
+      </main>
+    </>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        softBlue: "hsl(231, 69%, 60%)",
+        softRed: "hsl(0, 94%, 66%)",
+        grayishBlue: "hsl(229, 8%, 60%)",
+        veryDarkBlue: "hsl(229, 31%, 21%)",
+      },
+    },
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
+    },
+  },
+  plugins: [],
+};
